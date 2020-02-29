f894fb89e6b7082bf90f23e62cedcbd12349ecf4
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-02-29 13:33:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' openwrt/feeds.conf.default
+#sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/Lienol/openwrt-package
 git clone https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-vssr
 git clone https://github.com/Leo-Jo-My/my package/my

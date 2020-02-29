fe35c1424440e874acb08875834aacf7272ccc88
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-02-29 11:15:51 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,9 +7,21 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/Lienol/openwrt-package
-./scripts/feeds update -a
-./scripts/feeds install -a
-git clone https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-vssr
-git clone https://github.com/Leo-Jo-My/my package/my
-make package/luci-app-vssr/{clean,compile} V=s
+
+ - name: 涡轮增雕进程
+        run: |
+          cd package
+          mkdir openwrt-packages
+          cd openwrt-packages
+          git clone https://github.com/Lienol/openwrt-package.git
+          ./scripts/feeds update -a
+          ./scripts/feeds install -a
+
+ - name: 涡轮增雕进程
+        run: |
+          cd package
+          mkdir openwrt-packages
+          cd openwrt-packages
+          git clone https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-vssr
+          git clone https://github.com/Leo-Jo-My/my package/my
+          make package/luci-app-vssr/{clean,compile} V=s

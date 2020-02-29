a2337f8db86a8941d8f57b0e9d7c4a3e93a49f3a
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-02-29 10:48:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,9 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Lienol/openwrt-package
+./scripts/feeds update -a
+./scripts/feeds install -a
+git clone https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/Leo-Jo-My/my package/my
+make package/luci-app-vssr/{clean,compile} V=s

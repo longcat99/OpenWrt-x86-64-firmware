49dbc731af70ab7a6671bcc80554485994339f42
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-03-03 20:13:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,6 +10,8 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 cat >> feeds.conf.default<<EOF
 > src-git lienol https://github.com/Lienol/openwrt-package
 >EOF
+run: cd openwrt && ./scripts/feeds update -a
+run: cd openwrt && ./scripts/feeds install -a
 #sed '3 a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' work/OpenWrt-x86-64-firmware/OpenWrt-x86-64-firmware/openwrt/feeds.conf.default
 #git clone https://github.com/Lienol/openwrt-package

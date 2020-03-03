305404f9904f1b3fea3ca0e1c2cfa2a17da53712
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-03-03 21:00:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,10 +9,8 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 #sed '3 a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-run: cd openwrt && ./scripts/feeds update -a
-run: cd openwrt && ./scripts/feeds install -a
 #git clone https://github.com/Lienol/openwrt-package
-#./scripts/feeds update -a
-#./scripts/feeds install -a
+./scripts/feeds update -a
+./scripts/feeds install -a
 #git clone https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-vssr
 #git clone https://github.com/Leo-Jo-My/my package/my

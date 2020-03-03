9ef509b91192841fa586f6d0c471496eb32fd138
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-03-03 22:43:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,6 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 #＃添加其他软件包
 git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-Jo
-git clone https://github.com/Leo-Jo-My/my package/my
+#git clone https://github.com/Leo-Jo-My/my package/my
 git clone https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-vssr
-git clone https://github.com/Leo-Jo-My/my package/my
+#git clone https://github.com/Leo-Jo-My/my package/my

a07bab01de7264f81dfd9ab7d240265eade7437a
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-03-03 19:30:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,8 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-#sed -i '3a\src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '/src-git routing https://git.openwrt.org/feed/routing.git;openwrt-19.07/a\src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed '3 a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' work/OpenWrt-x86-64-firmware/OpenWrt-x86-64-firmware/openwrt/feeds.conf.default
 #git clone https://github.com/Lienol/openwrt-package
 #./scripts/feeds update -a

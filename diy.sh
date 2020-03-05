c4e9b4f5318a645d28474b7f8374a4cce55fc712
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-03-06 01:49:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -35,3 +35,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
         #EOF
 
 #==================================================================================
+
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome              package/adg
+#git clone https://github.com/vernesong/OpenClash                                      package/clash
+

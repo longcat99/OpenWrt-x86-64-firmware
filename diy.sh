3000c27f7901eb46226c652e90843f6c4a83b359
dannyjoey <50540465+dannyjoey@users.noreply.github.com>
2020-03-06 01:46:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -16,3 +16,22 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 #git clone https://github.com/Leo-Jo-My/my package/my
 
 #make package/luci-app-vssr/{clean,compile} V=s
+
+#==================================================================================
+
+    #- name: 涡轮增雕进程
+      #run: |
+        #cd package
+        #mkdir openwrt-packages
+        #cd openwrt-packages
+        #git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+
+        # LuCI主题:
+        #cat >> .config <<EOF
+        #CONFIG_PACKAGE_luci-i18n-kcptun-zh-cn=y
+        #CONFIG_PACKAGE_luci-i18n-trojan-server-zh-cn=y
+        #CONFIG_PACKAGE_luci-app-kcptun=y
+        #CONFIG_PACKAGE_luci-i18n-trojan-server-zh-cn=y
+        #EOF
+
+#==================================================================================
